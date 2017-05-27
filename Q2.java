e37b624b0815d5e56afda93ce77214dbdd0b1b03
Arpita Biswas <arpita.biswas@live.in>
2017-05-27 12:38:05 +0530

    Updated solution for Q2

--- Q2.java
+++ Q2.java
@@ -7,6 +7,10 @@ class Result{
 		this.potentialAncestor = null;
 		this.isAncestor = false;
 	}
+	public Result(Node<Integer> potentialAncestor, boolean isAncestor){
+		this.potentialAncestor = potentialAncestor;
+		this.isAncestor = isAncestor;
+	}
 }
 
 public class Q2 {	
@@ -25,34 +29,31 @@ public class Q2 {
 	}
 	
 	static Result commonAncestor(Node<Integer> root, int p, int q){
-		Result res = new Result();
-		
+				
 		//if root is null, then return null node as potentialAncestor with isAncestor = false
 		if(root == null){
-			return res;
+			return new Result(null, false);
 		}
 		//System.out.println("Node: "+root.data);
 		
 		//Check if root contains p
 		if(root.data == p){//if root contains p, then search for q in the subtree
 			if(find(root, q)){//if q is found in the subtree, then return root with isAncestor = true
-				res.isAncestor = true;
+				return new Result (root, true);
 			}
 			//if q is not found in the subtree, then return root as potentialAncestor
 			//with isAncestor = false, indicating that this node contains either p or q
-			res.potentialAncestor = root;
-			return res;
+			return new Result (root, false);
 		}
 		
 		//Check if root contains q
 		if(root.data == q){//if root contains q, then search for q in the subtree			
 			if(find(root, p)){//if p is found in the subtree, then return root with isAncestor = true
-				res.isAncestor = true;
+				return new Result (root, true);
 			}
 			//if p is not found in the subtree, then return root as potentialAncestor
 			//with isAncestor = false, indicating that this node contains either p or q
-			res.potentialAncestor = root;
-			return res;
+			return new Result (root, false);
 		}
 		
 		//If root does not contain p or q, look at the left subtree for p or q
@@ -63,6 +64,7 @@ public class Q2 {
 			return leftRes;
 		}
 
+		//If common ancestor id not yet found in left subtree, look at right subtree for p or q
 		Result rightRes = commonAncestor(root.right, p, q);
 		//Check whether right subtree has already found the common ancestor
 		if(rightRes.isAncestor){//If yes, return the common ancestor obtained from right subtree
@@ -70,24 +72,21 @@ public class Q2 {
 		}
 		
 		//If common ancestor is not obtained from left or right subtree
-		//check if both as got p or q (the potentialAncestor field of Result captures it) 
+		//check whether both left and right has got either p or q (the potentialAncestor field of Result captures it) 
 		if(leftRes.potentialAncestor != null && rightRes.potentialAncestor != null){
-			//If both left and right subtree that one of p or q, then root is the common ancestor
-			res.isAncestor = true;
-			res.potentialAncestor = root;
-			return res;
+			//If both left and right subtree has one of p or q, then root is the common ancestor
+			return new Result(root, true);
 		}
 		
-		//Check if one of left or right has p or q
+		//Check whether any one of left or right has p or q
 		if(leftRes.potentialAncestor != null || rightRes.potentialAncestor != null){
 			//then root is a potentialAncestor
-			res.potentialAncestor = root;
-			return res;
+			return new Result(root, false);
 		}
 		
 		//If none of the above is true, then the root along with the subtree neither contain p nor q
 		//hence return null as potentialAncestor with isAncestor = false
-		return res;
+		return new Result(null, false);
 	}
 	
 	static boolean find(Node<Integer> root, int num){
@@ -102,10 +101,10 @@ public class Q2 {
 	
 	public static void main(String[] args) {
 		Tree t = new Tree();
-		int data[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+		int data[] = {16,9,18,3,14,Tree.NullNode,19,1,5};
 		t.populateTree(data);
 		t.printTree();
 		
-		findCommonAncestor(t,9,8);
+		findCommonAncestor(t,5,14);
 	}
 }
